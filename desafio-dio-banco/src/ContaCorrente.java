db8d24da9717199a2c2cbf7e09211273d360ac69
Gustavo Trevizan <gustavo-trevizan@hotmail.com>
2024-08-05 22:32:47 -0300

    Commit inicial sobre abstração e definição das classes

---
+++ desafio-dio-banco/src/ContaCorrente.java
@@ -0,0 +1,20 @@
+public class ContaCorrente {
+
+    private int agencia;
+    private int conta;
+    private double saldo;
+
+    public void sacar() {
+
+    }
+
+    public void depositar() {
+
+    }
+
+    public void transferir() {
+
+    }
+
+
+}
